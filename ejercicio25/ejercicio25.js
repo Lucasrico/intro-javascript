fc3a37a15c97c61c2d9e0b627899102063b7edd4
Lucas Rico <lucasfernandezrico22@gmail.com>
2023-10-21 13:08:12 +0200

    ej25

--- ejercicio25/ejercicio25.js
+++ ejercicio25/ejercicio25.js
@@ -11,8 +11,40 @@
  *
  *  La funcion debera retornar cada linea + un espacio en blanco '\n';
  */
-function crearCuadrado() {
-  //Escribe tu codigo aqui
+function crearCuadrado(ancho) {
+  const salto = "\n";
+
+  // funcion que modifique el valor de la figura para ir pintando sobre ella
+
+  function pintar(figura, ch, times) {
+    for (i = 0; i < times; i++) {
+      figura = figura + ch;
+    }
+    return figura;
+  }
+
+  //Pintamos la linea superior de ancho a;
+  //Pintamos a-2 lineas de: "*"" + (a-2)" " + "*";
+  //Pintamos la linea inferior igual que la superior;
+
+  //Le estamos pasando la misma figura como aprametro a la funcion pintar para que al asignarle ese valor
+  //a la antigua figura, construya sobre ella.
+
+  let cuadrado = "";
+  cuadrado = pintar(cuadrado, "*", ancho);
+  cuadrado = pintar(cuadrado, salto, 1);
+  for (let i = 0; i < ancho - 2; i++) {
+    cuadrado = pintar(cuadrado, "*", 1);
+    cuadrado = pintar(cuadrado, " ", ancho - 2);
+    cuadrado = pintar(cuadrado, "*", 1);
+    cuadrado = pintar(cuadrado, salto, 1);
+  }
+  cuadrado = pintar(cuadrado, "*", ancho);
+  cuadrado = pintar(cuadrado, salto, 1);
+
+  return cuadrado;
 }
+
+console.log(crearCuadrado(80));
 
 module.exports = { crearCuadrado };
